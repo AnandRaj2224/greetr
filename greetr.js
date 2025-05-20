a2ef920306c84507b9589a0c4aedacae68594e83
Anand <anandraj6783@gmail.com>
2025-05-20 21:15:09 +0530

    Encapsulation Using the IIFE

--- greetr.js
+++ greetr.js
@@ -1,3 +1,22 @@
 ( function (global) {
 
+  var Greetr = function (firstName, lastName, language) {
+    return new Greetr.init(firstName,lastName,language);
+  }
+
+    Greetr.prototype = {};
+     
+  Greetr.init = function (firstName,lastName,language) {
+
+    var self = this;
+
+    this.firstName = firstName || '';
+    this.lastName = lastName || '';
+    this.language = language || 'en';
+  }
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  Greetr.global = global.G$ = Greetr;
+
 } (window));
