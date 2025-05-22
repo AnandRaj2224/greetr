75e8884e7568babf8f486d89f50a78da9e2808a8
Anand <anandraj6783@gmail.com>
2025-05-22 22:26:42 +0530

    added methods to the library

--- greetr.js
+++ greetr.js
@@ -4,7 +4,70 @@
     return new Greetr.init(firstName,lastName,language);
   }
 
-    Greetr.prototype = {};
+  var supportedLang = ['en', 'hindi'];
+
+  var greetings  = {
+    en : 'hello',
+    hindi : 'namaste'
+  };
+
+  var formalGreetings = {
+    en : 'greetings',
+    hindi : 'namaskar'
+  };
+
+  var logMessages = {
+    en : 'logged in',
+    hindi : 'aap log in hain'
+  };
+
+  Greetr.prototype = {
+
+    fullName : function () {
+      return this.firstName + ' ' + this.lastName;
+    },
+    validate : function () {
+      if(supportedLang.indexOf(this.language) === -1) {
+        throw "invalid language";
+      }
+    },
+    greeting : function () {
+      return greetings[this.language] + ' ' + this.firstName + '!';
+    },
+    formalGreeting : function () {
+      return formalGreetings[this.language] + ' ' + this.fullName();
+    },
+    greet : function(formal) {
+      var msg;
+
+      if(formal) {
+        msg = this.formalGreeting();
+      }
+      else {
+        msg = this.greeting();
+      }
+
+      if(console) {
+        console.log(msg);
+
+        return this;
+      }
+    },
+
+    log : function () {
+      if(console) {
+        console.log(logMessages[this.language] + ':' + this.fullName());
+
+        return this;
+      }
+    },
+    setLang : function (lang) {
+      this.language = lang;
+      this.validate();
+      return this;
+    } 
+
+  };
      
   Greetr.init = function (firstName,lastName,language) {
 
